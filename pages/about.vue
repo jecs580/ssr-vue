23efc3e3e1a0b79b57d94f227df90289fba2c39e
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-05-25 23:47:46 -0400

    Agregando etiquetas en el head a las paginas desde vue

---
+++ pages/about.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>About Us</h1>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati
+      voluptate provident asperiores architecto repudiandae dignissimos
+      voluptatibus animi quos harum. Impedit rem reiciendis, quaerat amet
+      perferendis sit voluptate ullam possimus eaque.
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  head() {
+    return {
+      title: "Post | Home",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Website about posts",
+        },
+      ],
+      htmlAttrs: {
+        lang: "es",
+      },
+    };
+  },
+};
+</script>
