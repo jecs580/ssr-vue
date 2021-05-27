31c4c9c9d8f76fa77478df30b783713b23da8bd1
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-05-26 21:47:26 -0400

    Creacion de paginas para posts traidos desde jsonPlaceholder

---
+++ pages/posts/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Post</h1>
+        <ul>
+            <li v-for="(post,i) of posts" :key="i">{{post.title}}</li>
+        </ul>
+    </div>
+</template> 
+<script>
+import axios from 'axios'
+export default {
+    data(){
+        return {
+            posts:[]
+        }
+    },
+    async created(){
+        try {
+            const res= await axios.get('https://jsonplaceholder.typicode.com/posts');
+            this.posts = res.data;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+</script>
