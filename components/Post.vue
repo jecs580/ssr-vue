0f315262928099b4e10c2f33906f6a7267d629f4
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-05-28 23:06:50 -0400

    Listando de posts y agregando un enlace para ver el detalle del post

--- components/Post.vue
+++ components/Post.vue
@@ -1,7 +1,9 @@
 <template>
-    <div>
+  <nuxt-link :to="'/posts/'+id">
+        <div class="post">
         <p>{{id}} - {{title}}</p>
     </div>
+  </nuxt-link>
 </template>
 <script>
 export default {
@@ -9,3 +11,22 @@ export default {
     props:['id','title']
 }
 </script>
+<style>
+ .post{
+     padding: 1rem;
+     border: 1px solid whitesmoke;
+     margin: 1rem 0;
+ }
+ .post:hover{
+     background: #2d3436;
+     border: 1px solid #6c5ce7;
+     cursor: pointer;
+ }
+ a{
+     color: whitesmoke;
+     text-decoration: none;
+ }
+ a:hover{
+     color: #6c5ce7;
+ }
+</style>
---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Prueba Detail</h1>
+    </div>
+</template>
