f31ea8753679f40f3ede414414cc6bcd6e59d228
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-05-29 21:18:02 -0400

    Mostrando detalle de un post

---
+++ pages/posts/_id.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="card">
+        <h2>{{post.title}}</h2>
+        <p>Id: {{post.id}}</p>
+        <p>User: {{post.userId}}</p>
+        <p>Content: {{post.body}}</p>
+    </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+    data(){
+        return {
+            post:{}
+        }
+    },
+    async created() {
+        const id = this.$route.params.id;
+        const res = await axios.get('https://jsonplaceholder.typicode.com/posts/'+id);
+        this.post = res.data;
+    },
+}
+</script>
+<style>
+    .card{
+        border: 1px solid whitesmoke;
+        padding: 4rem;
+    }
+</style>
--- pages/posts/_id/index.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        <h1>Prueba Detail</h1>
-    </div>
-</template>
